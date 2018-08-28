10470c88f03bf884f9cb4c0ece10a900511ba797
FalinCat <igor.vanteev@gmail.com>
2018-08-28 18:11:12 +0300

    Относим проект на работу

--- app/src/main/java/com/example/falin/simpleweather/MainActivity.kt
+++ app/src/main/java/com/example/falin/simpleweather/MainActivity.kt
@@ -41,7 +41,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mDrawerLayout: DrawerLayout
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_root_layout)
@@ -52,21 +51,16 @@ class MainActivity : AppCompatActivity() {
 
         mDrawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
+
         navigationView.setNavigationItemSelectedListener { menuItem ->
             // set item as selected to persist highlight
             menuItem.isChecked = true
             // close drawer when item is tapped
             mDrawerLayout.closeDrawers()
 
-            // Add code here to update the UI based on the item selected
-            // For example, swap UI fragments here
-
+            TODO("Нет времени на доработку :(")
             true
         }
-
-
-
-
     }
 
     override fun onResume() {
@@ -138,7 +132,6 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
-
 
 
     }
